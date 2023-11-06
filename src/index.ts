38bf150fdbda6815af89966493bcb46f804570e3
net-tech <hello@nettech.dev>
2023-11-06 01:51:27 +0100

    feat: remove most caching

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,34 @@
 import { URL } from "node:url"
-import { Client, GatewayIntentBits } from "discord.js"
+import { Client, GatewayIntentBits, Options } from "discord.js"
 import { loadCommands, loadEvents } from "./util/loaders.js"
 import { registerEvents } from "./util/registerEvents.js"
 
 // Initialize the client
-const client = new Client({ intents: [GatewayIntentBits.Guilds] })
+const client = new Client({
+	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers],
+	makeCache: Options.cacheWithLimits({
+		AutoModerationRuleManager: 0,
+		UserManager: 10, // "... needs to have 10 or so for safety" - discord js people
+		PresenceManager: 0,
+		VoiceStateManager: 0,
+		ThreadMemberManager: 0,
+		StageInstanceManager: 0,
+		ReactionUserManager: 0,
+		MessageManager: 0,
+		GuildMemberManager: 0,
+		ReactionManager: 0,
+		GuildBanManager: 0,
+		GuildEmojiManager: 0,
+		GuildInviteManager: 0,
+		GuildScheduledEventManager: 0,
+		GuildStickerManager: 0,
+		BaseGuildEmojiManager: 0
+	}),
+	allowedMentions: {
+		parse: ["users"],
+		repliedUser: true
+	}
+})
 
 // Load the events and commands
 const events = await loadEvents(new URL("events/", import.meta.url))
@@ -14,4 +38,4 @@ const commands = await loadCommands(new URL("commands/", import.meta.url))
 registerEvents(commands, events, client)
 
 // Login to the client
-void client.login()
+client.login(Bun.env.DISCORD_TOKEN)
