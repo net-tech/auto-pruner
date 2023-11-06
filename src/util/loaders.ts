6ed303d37af8ab73d5f2b4dc475c89f3b38623e6
net-tech <hello@nettech.dev>
2023-11-06 01:52:58 +0100

    feat: remove comments and make it bun compatible

--- src/util/loaders.ts
+++ src/util/loaders.ts
@@ -5,6 +5,7 @@ import type { Command } from "../commands/index.js"
 import { predicate as commandPredicate } from "../commands/index.js"
 import type { Event } from "../events/index.js"
 import { predicate as eventPredicate } from "../events/index.js"
+import { logger } from "./logger.js"
 
 /**
  * A predicate to check if the structure is valid
@@ -24,42 +25,32 @@ export async function loadStructures<T>(
 	predicate: StructurePredicate<T>,
 	recursive = true
 ): Promise<T[]> {
-	// Get the stats of the directory
 	const statDir = await stat(dir)
 
-	// If the provided directory path is not a directory, throw an error
 	if (!statDir.isDirectory()) {
-		throw new Error(`The directory '${dir}' is not a directory.`)
+		logger.fatal(`The directory '${dir}' is not a directory.`)
+		process.exit(1)
 	}
 
-	// Get all the files in the directory
 	const files = await readdir(dir)
 
-	// Create an empty array to store the structures
 	const structures: T[] = []
 
-	// Loop through all the files in the directory
 	for (const file of files) {
-		// If the file is index.js or the file does not end with .js, skip the file
-		if (file === "index.js" || !file.endsWith(".js")) {
+		if (file === "index.ts" || !file.endsWith(".ts")) {
 			continue
 		}
 
-		// Get the stats of the file
 		const statFile = await stat(new URL(`${dir}/${file}`))
 
-		// If the file is a directory and recursive is true, recursively load the structures in the directory
 		if (statFile.isDirectory() && recursive) {
 			structures.push(
 				...(await loadStructures(`${dir}/${file}`, predicate, recursive))
 			)
 			continue
 		}
-
-		// Import the structure dynamically from the file
 		const structure = (await import(`${dir}/${file}`)).default
 
-		// If the structure is a valid structure, add it
 		if (predicate(structure)) structures.push(structure)
 	}
 
